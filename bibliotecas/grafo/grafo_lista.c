55c0007b3f1ced28f76d48b5306fa59c12644b88
WandersonGomes <dersom100@gmail.com>
2023-09-17 15:53:13 -0300

    Lista de Adjacencia.

---
+++ bibliotecas/grafo/grafo_lista.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../lista/lista.h"
+#include "grafo_lista.h"
+
+GrafoListaAdjacencia* criarGrafoListaAdjacencia(int quantidade_vertices) {
+    GrafoListaAdjacencia* grafo = (GrafoListaAdjacencia*) malloc(sizeof(GrafoListaAdjacencia));
+    
+    if (grafo != NULL) {
+        grafo->quantidade_vertices = quantidade_vertices;
+        grafo->lista_adjacencia = (Lista**) malloc(sizeof(Lista*) * quantidade_vertices);
+
+        for (int i = 0; i < quantidade_vertices; i++)
+            grafo->lista_adjacencia[i] = criarLista();
+    }
+    
+    return grafo;
+}
+
+void adicionarArestaGrafoMatrizIncidencia(GrafoListaAdjacencia* grafo, Aresta aresta) {
+    if (grafo->eh_direcionado) {
+        inserirListaOrdenadoCrescente(
+            grafo->lista_adjacencia[aresta.vertice_origem], 
+            criarDado(aresta.vertice_destino, aresta.peso)
+        );
+    } else {
+        inserirListaOrdenadoCrescente(
+            grafo->lista_adjacencia[aresta.vertice_origem], 
+            criarDado(aresta.vertice_destino, aresta.peso)
+        );
+
+        inserirListaOrdenadoCrescente(
+            grafo->lista_adjacencia[aresta.vertice_destino], 
+            criarDado(aresta.vertice_origem, aresta.peso)
+        );
+    }
+}
+
+void imprimirGrafoMatrizIncidencia(GrafoListaAdjacencia* grafo) {
+    printf("TIPO: ");
+    if (grafo->eh_direcionado)
+        puts("GRAFO DIRECIONADO (DIGRAFO)");
+    else 
+        puts("GRAFO NAO DIRECIONADO");
+    
+    printf("\nVERTICES: {");
+    for (int i = 1; i <= grafo->quantidade_vertices; i++)
+        (i != 1) ? printf(", %d", i) : printf("%d", i);
+    puts("}");
+
+    puts("\nLISTA ADJACENCIA:"); 
+    for (int i = 0; i < grafo->quantidade_vertices; i++)
+        grafo->lista_adjacencia[i];
+}
+
+void destruirGrafoListaAdjacencia(GrafoListaAdjacencia* grafo) {
+    for (int i = 0; i < grafo->quantidade_vertices; i++)
+        destruirLista(grafo->lista_adjacencia[i]);
+
+    free(grafo->lista_adjacencia);
+
+    free(grafo);
+}
+
---
+++ bibliotecas/grafo/grafo_lista.h
@@ -0,0 +1,23 @@
+#include "../lista/lista.h"
+
+#ifndef __GRAFO_LISTA__
+#define __GRAFO_LISTA__
+
+typedef struct {
+    int vertice_origem;
+    int vertice_destino;
+    int peso;
+} Aresta;
+
+typedef struct {
+    int eh_direcionado;
+    int quantidade_vertices;
+    Lista** lista_adjacencia;
+} GrafoListaAdjacencia;
+
+GrafoListaAdjacencia* criarGrafoListaAdjacencia(int quantidade_vertices);
+void adicionarArestaGrafoMatrizIncidencia(GrafoListaAdjacencia* grafo, Aresta aresta);
+void imprimirGrafoMatrizIncidencia(GrafoListaAdjacencia* grafo);
+void destruirGrafoMatrizIncidencia(GrafoListaAdjacencia* grafo);
+
+#endif
